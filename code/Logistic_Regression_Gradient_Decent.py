48eaa1a98c0dfdd0af40145ef48a63530933f237
jieling1010 <jieling1010@gmail.com>
2019-10-20 16:20:26 -0700

    updating some code and add a jupyter notebook file

--- code/Logistic_Regression_Gradient_Decent.py
+++ code/Logistic_Regression_Gradient_Decent.py
@@ -1,67 +1,73 @@
+
 # coding: utf-8
 
 # In[1]:
 
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
 from patsy import dmatrices
 
 
 # In[2]:
 
-data = pd.read_csv("../data/gradient_descent_data.csv")
+data = pd.read_csv("../data/gradient_descent_data.csv") 
 
 
-# In[3]:
+# In[4]:
 
 data.head()
 
 
 # In[9]:
 
-data.dtypes
+type(data.left[0])
 
 
-# #standard operation to make one hot encoding for non-digit features
+# In[10]:
+
+data.rename(columns={"average_montly_hours":"average_monthly_hours"}, inplace = True)
+
 
 # In[11]:
 
-y, X = dmatrices("left~satisfaction_level+last_evaluation+number_project+average_montly_hours+time_spend_company+Work_accident+promotion_last_5years+C(sales)+C(salary)"
-    , data
-    , return_type = "dataframe")
+data.head()
 
 
-# In[12]:
+# # Convert data to two Dataframe's with left as y, other columns as X, also encode the categorial features
 
-X.head()
+# In[16]:
 
-
-# In[13]:
-
-y.head()
+y, X = dmatrices("left~satisfaction_level+last_evaluation+number_project+average_monthly_hours+time_spend_company+Work_accident+promotion_last_5years+C(sales)+C(salary)", data, return_type="dataframe")
 
 
-# #make y, X numpy matrix and one d array for matrix multiplication
+# In[17]:
+
+type(X), type(y)
+
+
+# Convert X, y as numpy matrix and ndArray
 
 # In[18]:
 
 X = np.asmatrix(X)
 y = np.ravel(y)
+
+
+# In[19]:
+
 type(X), type(y)
 
 
-# #make all the features in [0, 1] range
+# # Data nomalization of X to ([0,1))
 
 # In[22]:
 
 for i in range(1, X.shape[1]):
-    colMin = X[:, i].min()
-    colMax = X[:, i].max()
-    X[:, i] = (X[:, i]  - colMin)/(colMax - colMin)
+    xmin = X[:, i].min()
+    xmax = X[:, i].max()
+    X[:, i] = (X[:,i] - xmin)/(xmax-xmin)
 
 
 # In[24]:
@@ -69,88 +75,55 @@ for i in range(1, X.shape[1]):
 X
 
 
-# #start doing gradient decent
-
-# In[25]:
-
-np.random.seed(1)
-
+# Create an array of randome number, the same size of the number of features
 
 # In[26]:
-# learning rate
-alpha = 1 
+
+np.random.seed(1)
+alpha = 1
+# create a initial beta, which is the arguments of the Logistic Regression model
+beta = np.random.randn(X.shape[1])
+beta                        
 
 
-# In[27]:
+# # Definition of gradient decent
 
-# randomly create a beta as lr assignment
-beta = np.random.randn(X.shape[1])   
+# In[34]:
 
-
-# In[44]:
-
-losses = []
-error_rates = []
-derives = []
-for T in range(1000):
+# do 500 steps of gradient decent
+for T in range(500):
+    # use the initial model(aguments) creatd above to get the first y
+    prob = np.array(1./(1+np.exp(-np.matmul(X, beta)))).ravel()
+    # zip the calculated y with real y
+    prob_y = list(zip(prob, y))
+    # calculate loss with Logistic Regression cross entropy loss function
+    loss = -sum([np.log(p) if y == 1 else np.log(1-p) for p, y in prob_y])/len(y)
     
-    # define loss function
-    prob = 1.0 / (1  + np.exp(-np.matmul(X, beta)))      # p = 1/1 + e ^ -bx
-    prob = np.array(prob).ravel()                        # turn p into one d array to zip with y
-    pyzip = list(zip(prob, y))
-    loss = -sum(np.log(p) if yi == 1 else np.log(1-p) for p, yi in pyzip )/len(y)
-    
-    # caluclate error rate, which is the REAL loss
-    error_rate = 0.0
+    # calculate error rate
+    error_rate = 0
     for i in range(len(y)):
-        if ((prob[i] > 0.5 and y[i] == 0) or (prob[i] <= 0  and y[i] == 1)) :
+        if ( prob[i] > 0.5 and y[i] == 0 ) or ( prob[i] <= 0.5 and y[i] == 1 ) :
             error_rate += 1
     error_rate /= len(y)
+     
+    # print progress once 5 steps
+    if T % 5 == 0:
+        print( "T = " + str(T) + " Loss = " + str(loss) + " error_rate = " + str(error_rate))
     
-    # derivation ( gradient decent)
+    # calculate Derivative
+    # df/db = sum[Xi * (yi' - yi)]
     deriv = np.zeros(X.shape[1])
     for i in range(len(y)):
-        #deriv += (prob[i] - y[i]) * X[i]
-        deriv += np.asarray(X[i,:]).ravel() * (prob[i] - y[i])   
-        
+        deriv += np.asarray(X[i,:]).ravel() * (prob[i] - y[i])
     deriv /= len(y)
     
-    
-    # debug
-    losses.append(loss)
-    error_rates.append(error_rate)
-    derives.append(deriv[0])
-    
-    if T % 50 == 0 :
-        print("T = " + str(T) + ", loss = " + str(loss) + ", error rate = " + str(error_rate))
-        print("deriv = " + str(deriv))
-    # follow the oppsite direction of derivation to change the beta
-    beta -= alpha * deriv 
-    
+    # update the beta on the oppsite direction of derivative
+    beta = beta - deriv
 
 
-# In[50]:
-
-type(losses)
-
-
-# In[53]:
-
-x = range(1000)
-plt.plot(x, losses)
-
-
-# In[54]:
-
-plt.plot(x, error_rates)
-
-
-# In[52]:
-
-plt.plot(x, derives)
-
+# ending Loss = 0.43154384232365245 error_rate = 0.2058803920261351
 
 # In[ ]:
 
-# the best way to get the best arguments is to find the best accuracy on validation set
+
 
